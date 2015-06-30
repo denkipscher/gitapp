3d80292137564d904f602c7131f01f3fb602ffb3
denkipscher <denkipscher@gmail.com>
2015-06-30 02:27:28 -0700

    initial commit

---
+++ Appsclass.java
@@ -0,0 +1,17 @@
+package application;
+
+public class Appsclass {
+   public static void main(String[] args) { 
+
+	 printsomething("Hello word");
+	 
+}
+  public static void printsomething(String text) {
+	  System.out.println(text);
+  }
+  public static void computesum(int num1, int num2){
+	 int sum = num1 + num2;
+	 System.out.println("the sum is " + sum );
+			 
+  }
+}
